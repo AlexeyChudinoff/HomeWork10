8a58a62c9235efa148e38cfe062b13949523967b
AlexeyChudinoff <201900@mail.ru>
2024-10-26 23:39:59 +0700

    HomeWork10_00

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int nomerZadania = 01;
+    System.out.println();
+    System.out.println("Задание  " + nomerZadania++);// следующий номер задания
+
+
+
+
+    System.out.println();
+    System.out.println("Задание  " + nomerZadania++);
+  }
+}
